c2ff77eabb2e67dfd2bd779cc1077e4ad9e9e9c6
devs-cassiano <devs.cassiano@gmail.com>
2025-07-14 11:12:09 -0300

    fix: restrições de CORS removidas

--- src/app.js
+++ src/app.js
@@ -14,7 +14,11 @@ const micDtaRoutes = require('./routes/micDtaRoutes');
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 
 // Swagger Documentation
